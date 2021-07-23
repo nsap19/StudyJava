624256a765eddcaa92b84f430da540f74180d25f
nsap19 <viphyemin@gmail.com>
2021-07-23 20:03:13 +0900

    test

---
+++ src/hyemin/test.java
@@ -0,0 +1,11 @@
+package hyemin;
+
+import java.io.*;
+
+public class test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    }
+}
