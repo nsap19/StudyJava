7a4e6a74e964ee4731ccbbee35aace760bb712c0
nsap19 <viphyemin@gmail.com>
2021-07-23 20:19:12 +0900

    test

--- src/hyemin/test.java
+++ src/hyemin/test.java
@@ -6,6 +6,6 @@ public class test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        System.out.println("");
     }
 }
