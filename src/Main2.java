939900718ce281364cea9d996e1b2c83213e430c
nsap19 <viphyemin@gmail.com>
2021-07-24 16:36:05 +0900

    file clean

--- src/Main2.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class Main2 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    }
-}
