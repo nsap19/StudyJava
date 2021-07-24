a8cfbd6a027c647be034d2ebe822c7e9a664c781
nsap19 <viphyemin@gmail.com>
2021-07-24 16:36:42 +0900

    #10 기능개발

---
+++ src/hyemin/Programmers42586.java
@@ -0,0 +1,58 @@
+package hyemin;
+
+//progresses	speeds	return
+//[93, 30, 55]	[1, 30, 5]	[2, 1]
+//[95, 90, 99, 99, 80, 99]	[1, 1, 1, 1, 1, 1]	[1, 3, 2]
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Programmers42586 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(Solution.solution(new int[]{93, 30, 55}, new int[]{1, 30, 5})));
+        System.out.println(Arrays.toString(Solution.solution(new int[]{95, 90, 99, 99, 80, 99}, new int[]{1, 1, 1, 1, 1, 1})));
+    }
+
+}
+
+class Solution {
+    static public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> resultList = new ArrayList<>();
+        int[] result;
+
+        for (int i = 0; i < progresses.length; i++) {
+            int pDay = (int) Math.ceil((double) (100 - progresses[i]) / speeds[i]); //작업 기간
+
+            queue.add(pDay);
+        }
+
+        int day = queue.peek();
+        int count = 0;
+        while (!queue.isEmpty()) {
+            while(!queue.isEmpty() && queue.peek() <= day){ //첫번째 기능이 배포되는 날에 같이 배포할 수 있는 기능 갯수 count
+                count++;
+                queue.poll();
+            }
+
+            //더이상 같은 날에 배포할 수 있는 기능이 없다면 day+1
+            day++;
+
+
+            if (count != 0) {
+                resultList.add(count);
+                count = 0;
+            }
+            System.out.println(resultList.toString());
+        }
+
+        result = new int[resultList.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = resultList.get(i);
+        }
+
+        return result;
+    }
+}
