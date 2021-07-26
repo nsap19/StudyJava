dad69b6dbab5d6227c9900f1c4f0893b62755647
Talia2019 <chung1997jy@gmail.com>
2021-07-26 20:15:16 +0900

    #9 common assignment no.2

--- src/jiyoung/week1/Problem1.java
+++ src/jiyoung/week1/Problem1.java
@@ -3,7 +3,7 @@ package jiyoung.week1;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Solution {
+public class Problem1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
