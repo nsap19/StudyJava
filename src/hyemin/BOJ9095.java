e5b6d1aee2cb77970a457af3f544e523e292293c
nsap19 <viphyemin@gmail.com>
2021-08-17 21:33:56 +0900

    #30 1, 2, 3 더하기

---
+++ src/hyemin/BOJ9095.java
@@ -0,0 +1,46 @@
+package hyemin;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ9095 {
+    static int[] numbers = new int[]{1, 2, 3};
+    static int total, N;
+    static int[] result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int testCase = 0; testCase < T; testCase++) {
+            int num = Integer.parseInt(br.readLine());
+            total = 0;
+
+            for (int i = 1; i <= num; i++) {
+                N = i;
+                result = new int[i];
+                permutationa(0, num);
+            }
+            sb.append(total + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void permutationa(int cnt, int res) { //중복순열
+        if (cnt == N && res == 0) {
+            total++;
+//            System.out.println(Arrays.toString(result));
+            return;
+        } else if (cnt == N && res != 0) return;
+
+
+        for (int i = 0; i < numbers.length; i++) {
+            result[cnt] = numbers[i];
+            permutationa(cnt + 1, res - numbers[i]);
+        }
+
+
+    }
+}
