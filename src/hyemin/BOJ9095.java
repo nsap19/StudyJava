54433e9c5a509e5540d532f3b311cce488ccb109
nsap19 <viphyemin@gmail.com>
2021-08-17 21:38:52 +0900

    주석 추가

--- src/hyemin/BOJ9095.java
+++ src/hyemin/BOJ9095.java
@@ -20,7 +20,7 @@ public class BOJ9095 {
 
             for (int i = 1; i <= num; i++) {
                 N = i;
-                result = new int[i];
+                result = new int[i]; //1개부터 number개(모두 1로 더한 것)의 자리까지 중복순열 생성
                 permutationa(0, num);
             }
             sb.append(total + "\n");
@@ -33,7 +33,7 @@ public class BOJ9095 {
             total++;
 //            System.out.println(Arrays.toString(result));
             return;
-        } else if (cnt == N && res != 0) return;
+        } else if (cnt == N && res != 0) return; //모든 순열을 돌았지만 number가 만들어지지 않는 경우
 
 
         for (int i = 0; i < numbers.length; i++) {
