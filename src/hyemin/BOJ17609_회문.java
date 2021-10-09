4b1fd5d4beec345807b82b120adf6ececf3e19bd
nsap19 <viphyemin@gmail.com>
2021-10-10 00:10:04 +0900

    #77 회문

--- src/hyemin/BOJ17609_회문.java
+++ src/hyemin/BOJ17609_회문.java
@@ -8,5 +8,27 @@ public class BOJ17609_회문 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        String[][] arr = new String[N][];
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine().split("");
+        }
+
+        // 회문 0
+        // 유사회문 1
+        // 그 외 2
+        for (int i = 0; i < N; i++) {
+            String[] str = arr[i];
+            int left = 0, right = str.length - 1;
+            boolean flag = true;
+            while (left < right && str[left].equals(str[right])) {
+                left++;
+                right--;
+            }
+            if(left>right) flag= true;
+            else if(str[left].equals(str[right])){}
+
+        }
     }
 }
