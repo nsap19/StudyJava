91a928971702f19d843f09fcae15579f944e5e3c
nsap19 <viphyemin@gmail.com>
2021-10-20 23:15:29 +0900

    #84 Bureaucracy-위상정렬 시도(fail)

---
+++ src/hyemin/BOJ3551_Bureaucracy.java
@@ -0,0 +1,12 @@
+package hyemin;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ3551_Bureaucracy {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+    }
+}
