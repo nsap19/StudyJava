2ea3684165e8d559e1e73e216e61070a34755e7a
jy <chung1997jy@gmail.com>
2021-11-04 20:57:49 +0900

    phone

---
+++ src/jiyoung/week15/Phone.java
@@ -0,0 +1,19 @@
+package jiyoung.week15;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Phone {
+
+	// P4 5670 휴대폰자판
+	
+	// 자식개수가 하나면 자동입력
+	// 자식에 터미널이 여러개면 안됨
+	
+	// ㅠㅠㅠ시간이없다
+
+	public static void main(String[] args) {
+		
+	}
+
+}
