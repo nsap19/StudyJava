f47ba5544b7f01a69838ffd56d1d453d80705aa7
jy <chung1997jy@gmail.com>
2021-11-04 20:58:27 +0900

    phone

--- src/jiyoung/week15/Phone.java
+++ src/jiyoung/week15/Phone.java
@@ -10,7 +10,45 @@ public class Phone {
 	// 자식개수가 하나면 자동입력
 	// 자식에 터미널이 여러개면 안됨
 	
-	// ㅠㅠㅠ시간이없다
+	// ㅠㅠㅠ시간이 없다
+	
+	static class Trie {
+		class Node {
+			Map<Character, Node> child = new HashMap<>();
+			boolean isTerminal = false;
+		}
+
+		Node root;
+
+		public Trie() {
+			root = new Node();
+		}
+
+		public void insert(String str) {
+			int len = str.length();
+			Node cur = root;
+
+			for (int i = 0; i < len; i++) {
+				char c = str.charAt(i);
+				cur.child.putIfAbsent(c, new Node());
+				cur = cur.child.get(c);
+			}
+			cur.isTerminal = true;
+		}
+
+		public boolean find(String str) {
+			int len = str.length();
+			Node cur = root;
+
+			for (int i = 0; i < len; i++) {
+				char c = str.charAt(i);
+				if (cur.child.getOrDefault(c, null) == null)
+					return false;
+				cur = cur.child.get(c);
+			}
+			return true;
+		}
+	}
 
 	public static void main(String[] args) {
 		
